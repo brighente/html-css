d939db7b983c158b98ae333b9da7e4e2159887fa
João Vitor Brighente <jvbrighs@gmail.com>
2024-10-15 11:53:42 -0300

    a

---
+++ start-web/alto-valor(js)/script.js
@@ -0,0 +1,10 @@
+// var e let = possuem a mesma função, porém let é o mais utilizado atualmente
+// const = também declara variável, porém ela não pode ser alterada no decorrer do código
+
+let frase = 'Uma frase qualquer';
+
+const abacate = 9;
+
+alert(frase)
+
+alert(abacate)
