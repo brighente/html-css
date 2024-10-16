5a457f2b52dbdb1b55ff1b94527677225f372399
João Vitor Brighente <jvbrighs@gmail.com>
2024-10-16 08:59:41 -0300

    Update script.js

--- start-web/alto-valor(js)/script.js
+++ start-web/alto-valor(js)/script.js
@@ -1,5 +1,22 @@
 // var e let = possuem a mesma função, porém let é o mais utilizado atualmente
+
 // const = também declara variável, porém ela não pode ser alterada no decorrer do código
+
+/* 
+    Regras de variáveis: 
+    Não podem conter caracteres especiais, (exceto $ e _);
+    Não podem começar com numeros, mas pode ter no meio dela;
+    Variáveis são Case Sensitives;
+
+
+    *Dica*
+    Usar Cammel Case -> fraseQualquer timeBom;
+
+
+
+*/
+
+
 
 let frase = 'Uma frase qualquer';
 
